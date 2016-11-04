a39eb01455020174068943b63a5a7cad9b2335d3
Tal Risin <srisint@campus.technion.ac.il>
2016-11-04 20:51:37 +0200

    Pretty much done Now we just need to do all the checks thingy

--- MMX_AVX_Sol/Source.cpp
+++ MMX_AVX_Sol/Source.cpp
@@ -3,10 +3,16 @@
 #include <immintrin.h>
 #include <intrin.h>
 #include <iomanip>
+#include <string>
+#include <chrono>
 
 #define VECTOR_SIZE 1024
-
+#define LOOPS 100000
 int main() {
+	bool flag = false;
+	int Tot1 = 0, Tot2 = 0, Tot3 = 0;
+	int runs = 0;
+
 
 	__declspec(align(32))float A[VECTOR_SIZE];
 	__declspec(align(32))float B[VECTOR_SIZE];
@@ -16,6 +22,8 @@ int main() {
 
 	std::random_device rand;
 	std::mt19937 e2(rand());
+
+	start_fresh:
 
 	std::uniform_real_distribution<float> dist(0, 1);
 	for (int i = 0; i < VECTOR_SIZE; i++)
@@ -29,48 +37,95 @@ int main() {
 		B[i] = dist(e2);
 	}
 
+	start_same:
+
+	std::chrono::high_resolution_clock::time_point L1S = std::chrono::high_resolution_clock::now();
 	//Scalar
-	for (int i = 0; i < VECTOR_SIZE; i++)
+	for (int j = 0; j < LOOPS; j++)
 	{
-		C1[i] = A[i] + A[i]*B[i];
+		for (int i = 0; i < VECTOR_SIZE; i++)
+		{
+			C1[i] = A[i] + A[i] * B[i];
+		}
 	}
+	std::chrono::high_resolution_clock::time_point L1E = std::chrono::high_resolution_clock::now();
 
 	//Vector AVX2
 	__m256 ymmA, ymmB, ymmC;
-	for (int i = 0; i < VECTOR_SIZE; i += 8)
-	{
-		ymmA = _mm256_load_ps(A+i);
-		ymmB = _mm256_load_ps(B+i);
-		ymmC = _mm256_mul_ps(ymmA, ymmB);
-		ymmC = _mm256_add_ps(ymmA, ymmC);
-		
-		_mm256_store_ps(C2+i, ymmC);
-	}
 
-	for (int i = 0; i < VECTOR_SIZE; i += 8)
-	{
-		ymmA = _mm256_load_ps(A+i);
-		ymmB = _mm256_load_ps(B+i);
-		ymmC = _mm256_fmadd_ps(ymmA, ymmB, ymmA);
+	std::chrono::high_resolution_clock::time_point L2S = std::chrono::high_resolution_clock::now();
+	for (int j = 0; j < LOOPS; j++) {
+		for (int i = 0; i < VECTOR_SIZE; i += 8)
+		{
+			ymmA = _mm256_load_ps(A + i);
+			ymmB = _mm256_load_ps(B + i);
+			ymmC = _mm256_mul_ps(ymmA, ymmB);
+			ymmC = _mm256_add_ps(ymmA, ymmC);
 
-		_mm256_store_ps(C3+i, ymmC);
+			_mm256_store_ps(C2 + i, ymmC);
+		}
 	}
-	std::cout << std::setprecision(15) << std::fixed;
+	std::chrono::high_resolution_clock::time_point L2E = std::chrono::high_resolution_clock::now();
+
+	//Vector AVX2 + FMA
+	std::chrono::high_resolution_clock::time_point L3S = std::chrono::high_resolution_clock::now();
+	for (int j = 0; j < LOOPS; j++) {
+		for (int i = 0; i < VECTOR_SIZE; i += 8)
+		{
+			ymmA = _mm256_load_ps(A + i);
+			ymmB = _mm256_load_ps(B + i);
+			ymmC = _mm256_fmadd_ps(ymmB, ymmA, ymmA);
+
+			_mm256_store_ps(C3 + i, ymmC);
+		}
+	}
+	std::chrono::high_resolution_clock::time_point L3E = std::chrono::high_resolution_clock::now();
+
+	std::cout << std::setprecision(9) << std::fixed;
+	std::cout << "All differences between outputs:" << std::endl;
 	for (int i = 0; i < VECTOR_SIZE; i++)
 	{
-		if (C1[i] != C2[i])
+		if (C1[i] != C2[i] || C2[i] != C3[i] || C3[i] != C2[i])
 		{
-			std::cout << "C1[" << i << "]: " << C1[i] << "	but C2[" << i << "]: " << C2[i] << std::endl;
-		}
-		if (C2[i] != C3[i])
-		{
-			std::cout << "C2[" << i << "]: " << C2[i] << "	but C3[" << i << "]: " << C3[i] << std::endl;
-		}
-		if (C3[i] != C1[i])
-		{
-			std::cout << "C3[" << i << "]: " << C3[i] << "	but C1[" << i << "]: " << C1[i] << std::endl;
+			std::cout << "C1[" << i << "]: " << C1[i] << "	C2[" << i << "]: " << C2[i] << "	C3[" << i << "]: " << C3[i] << std::endl;
 		}
 	}
+	int Dur1, Dur2, Dur3;
+	Dur1 = std::chrono::duration_cast<std::chrono::milliseconds>(L1E - L1S).count();
+	Dur2 = std::chrono::duration_cast<std::chrono::milliseconds>(L2E - L2S).count();
+	Dur3 = std::chrono::duration_cast<std::chrono::milliseconds>(L3E - L3S).count();
+	Tot1 += Dur1;
+	Tot2 += Dur2;
+	Tot3 += Dur3;
+	runs++;
 
-	system("pause");
+	std::cout << "\n\n\n\nTotal execution time for " << LOOPS << " loops:" << std::endl;
+	std::cout << "Normal " << Dur1 << " milliseconds" << std::endl;
+	std::cout << "Vectoric (AVX2) " << Dur2 << " milliseconds" << std::endl;
+	std::cout << "Vectoric (AVX2+FMA) " << Dur3 << " milliseconds\n\n\n\n" << std::endl;
+
+	if (flag)
+	{
+		std::cout << "Total average execution time for " << LOOPS << " loops:" << std::endl;
+		std::cout << "Normal " << Tot1/runs << " milliseconds" << std::endl;
+		std::cout << "Vectoric (AVX2) " << Tot2/runs << " milliseconds" << std::endl;
+		std::cout << "Vectoric (AVX2+FMA) " << Tot3/runs << " milliseconds\n\n\n\n" << std::endl;
+	}
+
+	std::cout << "To re-run with same arrays, select R" << std::endl;
+	std::cout << "To re-run with new arrays, select N" << std::endl;
+	std::cout << "To exit, just hit enter..." << std::endl;
+
+	std::string input;
+	std::getline(std::cin,input);
+
+	if (input == "R") {
+		flag = true;
+		goto start_same;
+	}
+		
+	if (input == "N") {
+		flag = true;
+		goto start_fresh;
+	}
 }
